e1fba4626484622c3248e5995a4d20a8bea7fba8
[Lampart] Vi Hoang Son <hoang_son@lampart-vn.com>
2019-06-05 09:56:34 +0700

    Commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,8 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Tool UML - Lampart') }}</title>
+    {{--<title>{{ config('app.name', 'Tool UML - Lampart') }}</title>--}}
+    <title>Tool UML</title>
 
     <!-- Scripts -->
     {{--<script src="{{ asset('js/app.js') }}" defer></script>--}}
