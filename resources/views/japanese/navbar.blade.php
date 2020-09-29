7e87f994d6f126ed74b39a8f7115e959c6c63766
lampart_vi_hoang_son <vihoangson@gmail.com>
2020-09-30 00:11:01 +0700

    update

--- resources/views/japanese/navbar.blade.php
+++ resources/views/japanese/navbar.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand hidden" href="#">Chủ động trả giá</a>
+    <a class="navbar-brand d-none" href="#">Chủ động trả giá</a>
     <a class="navbar-brand" href="#">Japanese - Đông Du - PC 1A</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
