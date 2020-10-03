4227177cc935d2570c59b29e3ec796cecf565945
lampart_vi_hoang_son <vihoangson@gmail.com>
2020-10-03 16:11:47 +0700

    update

--- resources/views/japanese/audio.blade.php
+++ resources/views/japanese/audio.blade.php
@@ -31,6 +31,71 @@
                     id="audio-repeat-source">
             Your browser does not support the audio element.
         </audio>
+
+        <hr>
+
+        <h2>Other audio</h2>
+
+        <h3>04 Track 4.mp3</h3>
+        <audio controls id="audio-repeat">
+            <source src="/storage\audio_listen_sensei\04 Track 4.mp3" type="audio/mpeg"
+                    id="audio-repeat-source">
+            Your browser does not support the audio element.
+        </audio>
+        <h3>05 Track 5.mp3</h3>
+        <audio controls id="audio-repeat">
+            <source src="/storage\audio_listen_sensei\05 Track 5.mp3" type="audio/mpeg"
+                    id="audio-repeat-source">
+            Your browser does not support the audio element.
+        </audio>
+        <h3>06 Track 6.mp3</h3>
+        <audio controls id="audio-repeat">
+            <source src="/storage\audio_listen_sensei\06 Track 6.mp3" type="audio/mpeg"
+                    id="audio-repeat-source">
+            Your browser does not support the audio element.
+        </audio>
+        <h3>07 Track 7.mp3</h3>
+        <audio controls id="audio-repeat">
+            <source src="/storage\audio_listen_sensei\07 Track 7.mp3" type="audio/mpeg"
+                    id="audio-repeat-source">
+            Your browser does not support the audio element.
+        </audio>
+        <h3>08 Track 8.mp3</h3>
+        <audio controls id="audio-repeat">
+            <source src="/storage\audio_listen_sensei\08 Track 8.mp3" type="audio/mpeg"
+                    id="audio-repeat-source">
+            Your browser does not support the audio element.
+        </audio>
+        <h3>09 Track 9.mp3</h3>
+        <audio controls id="audio-repeat">
+            <source src="/storage\audio_listen_sensei\09 Track 9.mp3" type="audio/mpeg"
+                    id="audio-repeat-source">
+            Your browser does not support the audio element.
+        </audio>
+        <h3>10 Track 10.mp3</h3>
+        <audio controls id="audio-repeat">
+            <source src="/storage\audio_listen_sensei\10 Track 10.mp3" type="audio/mpeg"
+                    id="audio-repeat-source">
+            Your browser does not support the audio element.
+        </audio>
+        <h3>11 Track 11.mp3</h3>
+        <audio controls id="audio-repeat">
+            <source src="/storage\audio_listen_sensei\11 Track 11.mp3" type="audio/mpeg"
+                    id="audio-repeat-source">
+            Your browser does not support the audio element.
+        </audio>
+        <h3>31 Track 31.mp3</h3>
+        <audio controls id="audio-repeat">
+            <source src="/storage\audio_listen_sensei\31 Track 31.mp3" type="audio/mpeg"
+                    id="audio-repeat-source">
+            Your browser does not support the audio element.
+        </audio>
+        <h3>FILE_20200912_130256_27 Track 27 1.mp3</h3>
+        <audio controls id="audio-repeat">
+            <source src="/storage\audio_listen_sensei\FILE_20200912_130256_27 Track 27 1.mp3" type="audio/mpeg"
+                    id="audio-repeat-source">
+            Your browser does not support the audio element.
+        </audio>
     </div>
 @endsection
 @section('footer')
