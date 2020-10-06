bcd212896639c3825865a55b55143e1f8621458d
[Lampart] Vi Hoang Son <hoang_son@lampart-vn.com>
2020-10-06 16:10:06 +0700

    update file token

--- tests/Unit/VocabularyServiceTest.php
+++ tests/Unit/VocabularyServiceTest.php
@@ -2,8 +2,7 @@
 
 namespace App\Services;
 
-
-
+use Redis;
 use Tests\TestCase;
 
 class VocabularyServiceTest extends TestCase {
@@ -12,6 +11,13 @@ class VocabularyServiceTest extends TestCase {
         $actual = VocabularyService::getAll();
         $this->assertIsArray($actual);
     }
+
+    public function testRedis() {
+        Redis::set('1', '1');
+        $actual = Redis::get('1');
+        $this->assertEquals('1', $actual);
+    }
+
 
     public function testGetByLesson() {
         $actual = VocabularyService::getByLesson(1);
