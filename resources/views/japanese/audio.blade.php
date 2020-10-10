c12ca75525de40f6c5f79f4d8d05c8a88b40d4a4
lampart_vi_hoang_son <vihoangson@gmail.com>
2020-10-10 12:10:42 +0700

    update

--- resources/views/japanese/audio.blade.php
+++ resources/views/japanese/audio.blade.php
@@ -6,25 +6,25 @@
     <div class="text-center">
 
         <h2>Lesson 1</h2>
-        <audio controls id="audio-repeat">
+        <audio style="width:90%" controls id="audio-repeat">
             <source src="/storage/audio_listen_sensei/L01-01.MP3" type="audio/mpeg"
                     id="audio-repeat-source">
             Your browser does not support the audio element.
         </audio>
         <h2>Lesson 2</h2>
-        <audio controls id="audio-repeat">
+        <audio style="width:90%" controls id="audio-repeat">
             <source src="/storage/audio_listen_sensei/L02-01.MP3" type="audio/mpeg"
                     id="audio-repeat-source">
             Your browser does not support the audio element.
         </audio>
         <h2>Lesson 3</h2>
-        <audio controls id="audio-repeat">
+        <audio style="width:90%" controls id="audio-repeat">
             <source src="/storage/audio_listen_sensei/L3-01.MP3" type="audio/mpeg"
                     id="audio-repeat-source">
             Your browser does not support the audio element.
         </audio>
         <h2>Lesson 4</h2>
-        <audio controls id="audio-repeat">
+        <audio style="width:90%" controls id="audio-repeat">
             <source src="/storage/audio_listen_sensei/L4-1.MP3" type="audio/mpeg"
                     id="audio-repeat-source">
             Your browser does not support the audio element.
@@ -35,61 +35,61 @@
         <h2>Other audio</h2>
 
         <h3>04 Track 4.mp3</h3>
-        <audio controls id="audio-repeat">
+        <audio style="width:90%" controls id="audio-repeat">
             <source src="/storage\audio_listen_sensei\04 Track 4.mp3" type="audio/mpeg"
                     id="audio-repeat-source">
             Your browser does not support the audio element.
         </audio>
         <h3>05 Track 5.mp3</h3>
-        <audio controls id="audio-repeat">
+        <audio style="width:90%" controls id="audio-repeat">
             <source src="/storage\audio_listen_sensei\05 Track 5.mp3" type="audio/mpeg"
                     id="audio-repeat-source">
             Your browser does not support the audio element.
         </audio>
         <h3>06 Track 6.mp3</h3>
-        <audio controls id="audio-repeat">
+        <audio style="width:90%" controls id="audio-repeat">
             <source src="/storage\audio_listen_sensei\06 Track 6.mp3" type="audio/mpeg"
                     id="audio-repeat-source">
             Your browser does not support the audio element.
         </audio>
         <h3>07 Track 7.mp3</h3>
-        <audio controls id="audio-repeat">
+        <audio style="width:90%" controls id="audio-repeat">
             <source src="/storage\audio_listen_sensei\07 Track 7.mp3" type="audio/mpeg"
                     id="audio-repeat-source">
             Your browser does not support the audio element.
         </audio>
         <h3>08 Track 8.mp3</h3>
-        <audio controls id="audio-repeat">
+        <audio style="width:90%" controls id="audio-repeat">
             <source src="/storage\audio_listen_sensei\08 Track 8.mp3" type="audio/mpeg"
                     id="audio-repeat-source">
             Your browser does not support the audio element.
         </audio>
         <h3>09 Track 9.mp3</h3>
-        <audio controls id="audio-repeat">
+        <audio style="width:90%" controls id="audio-repeat">
             <source src="/storage\audio_listen_sensei\09 Track 9.mp3" type="audio/mpeg"
                     id="audio-repeat-source">
             Your browser does not support the audio element.
         </audio>
         <h3>10 Track 10.mp3</h3>
-        <audio controls id="audio-repeat">
+        <audio style="width:90%" controls id="audio-repeat">
             <source src="/storage\audio_listen_sensei\10 Track 10.mp3" type="audio/mpeg"
                     id="audio-repeat-source">
             Your browser does not support the audio element.
         </audio>
         <h3>11 Track 11.mp3</h3>
-        <audio controls id="audio-repeat">
+        <audio style="width:90%" controls id="audio-repeat">
             <source src="/storage\audio_listen_sensei\11 Track 11.mp3" type="audio/mpeg"
                     id="audio-repeat-source">
             Your browser does not support the audio element.
         </audio>
         <h3>31 Track 31.mp3</h3>
-        <audio controls id="audio-repeat">
+        <audio style="width:90%" controls id="audio-repeat">
             <source src="/storage\audio_listen_sensei\31 Track 31.mp3" type="audio/mpeg"
                     id="audio-repeat-source">
             Your browser does not support the audio element.
         </audio>
         <h3>FILE_20200912_130256_27 Track 27 1.mp3</h3>
-        <audio controls id="audio-repeat">
+        <audio style="width:90%" controls id="audio-repeat">
             <source src="/storage\audio_listen_sensei\FILE_20200912_130256_27 Track 27 1.mp3" type="audio/mpeg"
                     id="audio-repeat-source">
             Your browser does not support the audio element.
