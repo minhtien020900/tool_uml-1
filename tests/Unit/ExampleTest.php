7f79372c604defa21cc80eeb2a404538172a2438
[Lampart] Vi Hoang Son <hoang_son@lampart-vn.com>
2020-10-13 09:53:07 +0700

    update file token

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -4,11 +4,28 @@ namespace Tests\Unit;
 
 use App\Http\Controllers\Japanese\ServiceGoogle;
 use Google_Service_Sheets_ValueRange;
+use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ExampleTest extends TestCase
 {
+
+    public function testName() {
+        $files = Storage::disk('s3')->allFiles();
+        foreach ($files as $f){
+            Storage::disk('s3')->delete($f);
+        }
+        // return;
+        for($i=0;$i<5;$i++){
+            echo $i.PHP_EOL;
+            $filePath = rand(0,10000).'/file_'.rand(0,1000000);
+            Storage::disk('s3')->put($filePath,'sss','public');
+            echo Storage::disk('s3')->get($filePath).PHP_EOL;
+
+        }
+    }
+
     /**
      * A basic test example.
      *
