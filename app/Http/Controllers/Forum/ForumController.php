1f17b299055b4bd945500885d6a50a78a6ef71da
lampart_vi_hoang_son <vihoangson@gmail.com>
2020-10-24 23:50:39 +0700

    update nav

--- app/Http/Controllers/Forum/ForumController.php
+++ app/Http/Controllers/Forum/ForumController.php
@@ -26,40 +26,53 @@ class ForumController extends Controller {
 
     public function index(Request $request) {
         $data['data'] = \App\Services\Forum::getParentForum();
-        return view('forum.index',$data);
-    }
-    public function forum(Request $request) {
-        $cat = $request->input('cat');
-        $dataCat = \App\Services\Forum::getDataCat($cat);
-        $dataThreadCat = \App\Services\Forum::getDataThreadCat($cat);
-        $data['dataCat'] = $dataCat;
-        $data['dataThreadCat'] = $dataThreadCat;
-        return view('forum.category',$data);
-    }
-    public function thread(Request $request) {
-        $threadid = $request->input('threadid');
 
-        $dataThreadData = \App\Services\Forum::getThreadData($threadid);
+        return view('forum.index', $data);
+    }
+
+    public function forum(Request $request) {
+        $cat                   = $request->input('cat');
+        $dataCat               = \App\Services\Forum::getDataCat($cat);
+        $dataThreadCat         = \App\Services\Forum::getDataThreadCat($cat);
+        $data['dataCat']       = $dataCat;
+        $data['dataThreadCat'] = $dataThreadCat;
+
+        return view('forum.category', $data);
+    }
+
+    public function thread(Request $request) {
+        $threadid       = $request->input('threadid');
+        $dataThreadData = Cache::remember('thread_'.$threadid, 10000, function () use ($threadid) {
+            return \App\Services\Forum::getThreadData($threadid);
+        });
+        //$dataThreadData = \App\Services\Forum::getThreadData($threadid);
 
         $data['dataThreadData'] = $dataThreadData;
 
-        return view('forum.thread',$data);
+        return view('forum.thread', $data);
     }
 
     public function getParentForum(Request $request) {
         return \App\Services\Forum::getParentForum();
     }
-    public function getThread(Request $request,$id) {
-        return Thread::where('forumid',$id)->get();
+
+    public function getThread(Request $request, $id) {
+        return Thread::where('forumid', $id)
+                     ->get();
+
         return $id;
-        return \App\Services\Forum::getParentForum();
-    }
-    public function getDetailThread(Request $request,$id) {
-        return Post::where('threadid',$id)->get();
-        return $id;
+
         return \App\Services\Forum::getParentForum();
     }
 
+    public function getDetailThread(Request $request, $id) {
+        return Post::where('threadid', $id)
+                   ->get();
+
+        return $id;
+
+        return \App\Services\Forum::getParentForum();
+    }
 
 
 }
