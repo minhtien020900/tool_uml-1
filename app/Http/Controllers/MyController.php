28896e2dbd61f33c7fbeebb05b42822df2623c29
[Lampart] Vi Hoang Son <hoang_son@lampart-vn.com>
2021-04-07 13:51:33 +0700

    update

---
+++ app/Http/Controllers/MyController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use package_examples\package_example_01\Controllers\InfomationController;
+
+class MyController extends InfomationController
+{
+
+}
